d00a702c722ea663a6cc539d4165b6bd970927c3
Argenis Leon <argenisleon@gmail.com>
2023-05-08 16:48:46 -0600

    Cosine similarity working

---
+++ embeddings/ada/cosine_similarity.js
@@ -0,0 +1,219 @@
+(async () => {
+  if (!("gpu" in navigator)) {
+    console.log(
+      "WebGPU is not supported. Enable chrome://flags/#enable-unsafe-webgpu flag."
+    );
+    return;
+  }
+
+  const adapter = await navigator.gpu.requestAdapter();
+  if (!adapter) {
+    console.log("Failed to get GPU adapter.");
+    return;
+  }
+  const device = await adapter.requestDevice();
+
+  // First Matrix
+
+  const firstMatrix = new Float32Array([
+    3 /* rows */,
+    4 /* columns */,
+    1,
+    2,
+    3,
+    4,
+    10,
+    20,
+    -30,
+    6,
+    9,
+    10,
+    11,
+    12
+
+  ]);
+
+  const gpuBufferFirstMatrix = device.createBuffer({
+    mappedAtCreation: true,
+    size: firstMatrix.byteLength,
+    usage: GPUBufferUsage.STORAGE
+  });
+  const arrayBufferFirstMatrix = gpuBufferFirstMatrix.getMappedRange();
+  new Float32Array(arrayBufferFirstMatrix).set(firstMatrix);
+  gpuBufferFirstMatrix.unmap();
+
+  // Second Matrix
+
+  const secondMatrix = new Float32Array([
+    1 /* rows */,
+    4 /* columns */,
+    1,
+    2,
+    3,
+    4,
+  ]);
+
+  const gpuBufferSecondMatrix = device.createBuffer({
+    mappedAtCreation: true,
+    size: secondMatrix.byteLength,
+    usage: GPUBufferUsage.STORAGE
+  });
+  const arrayBufferSecondMatrix = gpuBufferSecondMatrix.getMappedRange();
+  new Float32Array(arrayBufferSecondMatrix).set(secondMatrix);
+  gpuBufferSecondMatrix.unmap();
+
+  // Result Matrix
+
+  const resultMatrixBufferSize =
+  Float32Array.BYTES_PER_ELEMENT * (2 + firstMatrix[0] * secondMatrix[0]);
+
+  const resultMatrixBuffer = device.createBuffer({
+    size: resultMatrixBufferSize,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC
+  });
+
+  // Bind group layout and bind group
+
+  const bindGroupLayout = device.createBindGroupLayout({
+    entries: [
+      {
+        binding: 0,
+        visibility: GPUShaderStage.COMPUTE,
+        buffer: {
+          type: "read-only-storage"
+        }
+      },
+      {
+        binding: 1,
+        visibility: GPUShaderStage.COMPUTE,
+        buffer: {
+          type: "read-only-storage"
+        }
+      },
+      {
+        binding: 2,
+        visibility: GPUShaderStage.COMPUTE,
+        buffer: {
+          type: "storage"
+        }
+      }
+    ]
+  });
+
+  const bindGroup = device.createBindGroup({
+    layout: bindGroupLayout,
+    entries: [
+      {
+        binding: 0,
+        resource: {
+          buffer: gpuBufferFirstMatrix
+        }
+      },
+      {
+        binding: 1,
+        resource: {
+          buffer: gpuBufferSecondMatrix
+        }
+      },
+      {
+        binding: 2,
+        resource: {
+          buffer: resultMatrixBuffer
+        }
+      }
+    ]
+  });
+
+  // Compute shader code
+const shaderModule = device.createShaderModule({
+  code: `
+    struct Matrix {
+      size : vec2<f32>,
+      numbers: array<f32>,
+    }
+
+    @group(0) @binding(0) var<storage, read> firstMatrix : Matrix;
+    @group(0) @binding(1) var<storage, read> secondMatrix : Matrix;
+    @group(0) @binding(2) var<storage, read_write> resultMatrix : Matrix;
+
+    @compute @workgroup_size(8, 8)
+    fn main(@builtin(global_invocation_id) global_id : vec3<u32>) {
+      // Guard against out-of-bounds work group sizes
+      if (global_id.x >= u32(firstMatrix.size.x) || global_id.y >= u32(secondMatrix.size.x)) {
+        return;
+      }
+
+      resultMatrix.size = vec2(firstMatrix.size.x, secondMatrix.size.x);
+
+      let rowA = global_id.x;
+      let rowB = global_id.y;
+      var dotProduct = 0.0;
+      var normA = 0.0;
+      var normB = 0.0;
+
+      for (var i = 0u; i < u32(firstMatrix.size.y); i = i + 1u) {
+        let a = i + rowA * u32(firstMatrix.size.y);
+        let b = i + rowB * u32(secondMatrix.size.y);
+        dotProduct = dotProduct + firstMatrix.numbers[a] * secondMatrix.numbers[b];
+        normA = normA + firstMatrix.numbers[a] * firstMatrix.numbers[a];
+        normB = normB + secondMatrix.numbers[b] * secondMatrix.numbers[b];
+      }
+
+      normA = sqrt(normA);
+      normB = sqrt(normB);
+      let cosineSimilarity = dotProduct / (normA * normB);
+
+      let index = rowB + rowA * u32(secondMatrix.size.x);
+      resultMatrix.numbers[index] = cosineSimilarity;
+    }
+  `
+});
+
+  // Pipeline setup
+
+  const computePipeline = device.createComputePipeline({
+    layout: device.createPipelineLayout({
+      bindGroupLayouts: [bindGroupLayout]
+    }),
+    compute: {
+      module: shaderModule,
+      entryPoint: "main"
+    }
+  });
+
+  // Commands submission
+
+  const commandEncoder = device.createCommandEncoder();
+
+  const passEncoder = commandEncoder.beginComputePass();
+  passEncoder.setPipeline(computePipeline);
+  passEncoder.setBindGroup(0, bindGroup);
+  const workgroupCountX = Math.ceil(firstMatrix[0] / 8);
+  const workgroupCountY = Math.ceil(secondMatrix[1] / 8);
+  passEncoder.dispatchWorkgroups(workgroupCountX, workgroupCountY);
+  passEncoder.end();
+
+  // Get a GPU buffer for reading in an unmapped state.
+  const gpuReadBuffer = device.createBuffer({
+    size: resultMatrixBufferSize,
+    usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ
+  });
+
+  // Encode commands for copying buffer to buffer.
+  commandEncoder.copyBufferToBuffer(
+    resultMatrixBuffer /* source buffer */,
+    0 /* source offset */,
+    gpuReadBuffer /* destination buffer */,
+    0 /* destination offset */,
+    resultMatrixBufferSize /* size */
+  );
+
+  // Submit GPU commands.
+  const gpuCommands = commandEncoder.finish();
+  device.queue.submit([gpuCommands]);
+
+  // Read buffer.
+  await gpuReadBuffer.mapAsync(GPUMapMode.READ);
+  const arrayBuffer = gpuReadBuffer.getMappedRange();
+  console.log(new Float32Array(arrayBuffer));
+})();
